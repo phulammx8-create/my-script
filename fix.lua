8fdaa4a177cae2aa34bb7fb6b9d3cc32a69c6e38
phulammx8-create <phulammx8@gmail.com>
2026-07-05 15:08:53 +0700

    Create fix.lua

---
+++ fix.lua
@@ -0,0 +1,20 @@
+-- 1. Configuration
+local loaiHatGiong = "Apple" 
+
+-- 2. Find Garden
+local vuan = game.Workspace:FindFirstChild("Gardens") 
+
+-- 3. Auto Plant Function
+local function tuDongTrongCay()
+    for _, oDat in pairs(vuan:GetChildren()) do
+        if oDat:FindFirstChild("Owner") and oDat.Owner.Value == game.Players.LocalPlayer.Name then
+            if not oDat:FindFirstChild("Plant") then 
+                game:GetService("ReplicatedStorage").Events.PlantSeed:FireServer(oDat, loaiHatGiong)
+                task.wait(0.1) 
+            end
+        end
+    end
+end
+
+-- 4. Run
+tuDongTrongCay()
